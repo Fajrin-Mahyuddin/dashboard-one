a90a6bfec169eee8dc811d8bc242b0678ddb91d2
Fajrin Mahyuddin <fajrin.telkom12@gmail.com>
2020-12-17 11:35:49 +0800

    addign alt bug:

--- src/components/atoms/image/image.js
+++ src/components/atoms/image/image.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 function Image({ src, alt, ...rest }) {
-  return <img {...rest} src={src} />;
+  return <img {...rest} src={src} alt={alt} />;
 }
 
 export default Image;
