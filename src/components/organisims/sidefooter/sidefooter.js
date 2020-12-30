d5b159ea401dd934062e18947bf1dfb34af0da0f
Fajrin Mahyuddin <fajrin.telkom12@gmail.com>
2020-12-30 20:18:21 +0800

    add footer btn

--- src/components/organisims/sidefooter/sidefooter.js
+++ src/components/organisims/sidefooter/sidefooter.js
@@ -1,7 +1,11 @@
 import React from "react";
 
 function Sidefooter() {
-  return <div className="sidefooter">Ini footer side</div>;
+  return (
+    <div className="sidefooter">
+      <button className="btn primary">Sign-Out</button>
+    </div>
+  );
 }
 
 export default Sidefooter;
--- src/components/templates/sidebar/sidebar.js
+++ src/components/templates/sidebar/sidebar.js
@@ -1,13 +1,13 @@
 import React from "react";
 import "./sidebar.scss";
-import { Profile, Sidemenu } from "components";
+import { Profile, Sidemenu, Sidefooter } from "components";
 
 function Sidebar() {
   return (
     <div className="sidebar">
       <Profile />
       <Sidemenu />
-      {/* <Sidefooter /> */}
+      <Sidefooter />
     </div>
   );
 }
